b25fb30cb7a89053983ffd3c1ce5edfe3b0c94fa
robinvd95 <robinvd95@live.nl>
2024-03-19 14:16:45 +0100

    Dist

---
+++ example/myexample.js
@@ -0,0 +1,11 @@
+// const fs = require('fs');
+// const jstt = require('../dist/src/index');
+
+// async function compile() {
+
+//   const jsonFile = fs.readFileSync("example/readonly.json")
+//   const foo = await jstt.compile(JSON.parse(jsonFile))
+//   console.log(foo)
+// }
+
+// compile();
